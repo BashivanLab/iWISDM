b5a387c876cdfad837cb2244db8a3b24d6b339e5
RainyFields <xiaoxuan.lei.claire@gmail.com>
2024-01-13 17:15:31 -0500

    also calculate the total number of tree

--- count_random_tasks.py
+++ count_random_tasks.py
@@ -21,6 +21,7 @@ def sum_combinations(n, k):
 
 def count_tree_structures(depth, ng, nc, nb):
     # return the total number of tree structures and select operators
+    # for now only consider with 0/1 select operator
     if depth == 3:
         return ng*nc*nb, 2
     elif depth == 4:
@@ -42,14 +43,22 @@ if __name__ == "__main__":
     min_frames = 1
     max_frames = 6
     total_tasks = np.zeros((max_frames - min_frames, 4))
+    total_trees = np.zeros((max_frames - min_frames, 4))
 
     plt.figure()
     for n_frames in range(min_frames,max_frames):
         for i, depth in enumerate([3,4,7,8]):
             n_trees, ns = count_tree_structures(depth, n_getattr_operators, n_comparison_operators, n_boolean_operators)
+            total_trees[n_frames-1,i] = n_trees
             total_tasks[n_frames-1, i] = count_total_tasks(n_trees, ns, n_frames)
         plt.plot([3,4,7,8], total_tasks[n_frames-1], label = "n_frames %d" % n_frames)
     plt.legend()
     plt.ylabel("total tasks")
     plt.xlabel("depth")
     plt.savefig("/mnt/store1/xiaoxuan/sanity_check/count_random_task.png")
+
+    plt.figure()
+    plt.plot([3,4,7,8], total_trees[0,:])
+    plt.ylabel("total trees")
+    plt.xlabel("depth")
+    plt.savefig("/mnt/store1/xiaoxuan/sanity_check/count_random_trees.png")
